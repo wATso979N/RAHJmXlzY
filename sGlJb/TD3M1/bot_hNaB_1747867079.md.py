e9ed305f8371e11ed46c44f1cb652752e96c7ebb
wATso979N <telcos_byroad0a@icloud.com>
2025-05-22 00:38:02 +0200

    Change the file's logging configuration

---
+++ sGlJb/TD3M1/bot_hNaB_1747867079.md.py
@@ -0,0 +1,22 @@
+# In this file, you can set the configurations of the app.
+
+from src.utils.constants import DEBUG, ERROR, LLM_MODEL, OPENAI
+
+#config related to logging must have prefix LOG_
+LOG_LEVEL = 'ERROR'
+LOG_SELENIUM_LEVEL = ERROR
+LOG_TO_FILE = False
+LOG_TO_CONSOLE = False
+
+MINIMUM_WAIT_TIME_IN_SECONDS = 60
+
+JOB_APPLICATIONS_DIR = "job_applications"
+JOB_SUITABILITY_SCORE = 7
+
+JOB_MAX_APPLICATIONS = 5
+JOB_MIN_APPLICATIONS = 1
+
+LLM_MODEL_TYPE = 'openai'
+LLM_MODEL = 'gpt-4o-mini'
+# Only required for OLLAMA models
+LLM_API_URL = ''
